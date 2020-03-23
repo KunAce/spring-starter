9efd766a252930dd740e46e110b7707b29cba09d
KunAce <wxkace@gmail.com>
2020-03-22 17:28:03 -0700

    Added 'Book' constructors and beans

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,6 +3,17 @@ public class Book {
     private String name;
     private Double price;
 
+    // Constructors
+    public Book() {
+        System.out.println("-------book init-------");
+    }
+
+    public Book(Integer id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     // Getters and Setters
     public Integer getId() {
         return id;
