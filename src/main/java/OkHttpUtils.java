ada99d98af7e605501e1092a673e98e2bc0cb90f
KunAce <wxkace@gmail.com>
2020-03-24 00:35:47 -0700

    Added instance factory method to the comment

--- src/main/java/OkHttpUtils.java
+++ src/main/java/OkHttpUtils.java
@@ -1,5 +1,6 @@
 import okhttp3.OkHttpClient;
 
+// static factory
 public class OkHttpUtils {
     private static OkHttpClient OkHttpClient;
     public static OkHttpClient getInstance() {
@@ -9,3 +10,14 @@ public class OkHttpUtils {
         return OkHttpClient;
     }
 }
+
+// instance factory
+//public class OkHttpUtils {
+//    private OkHttpClient OkHttpClient;
+//    public OkHttpClient getInstance() {
+//        if (OkHttpClient == null) {
+//            OkHttpClient = new OkHttpClient.Builder().build();
+//        }
+//        return OkHttpClient;
+//    }
+//}
