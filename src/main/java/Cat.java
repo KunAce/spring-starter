41a107de8901f1950af833c031d3c1b77c723a2e
KunAce <wxkace@gmail.com>
2020-03-25 23:34:26 -0700

    Added a new class for injecting an object

---
+++ src/main/java/Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+    private String name;
+    private String color;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
