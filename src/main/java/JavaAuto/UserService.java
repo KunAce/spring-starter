89e768b16c1914d6489314fbef2970349be086e0
KunAce <wxkace@gmail.com>
2020-03-29 18:14:07 -0700

    A tiny note on '@Autowired' on injection when auto-scanning

--- src/main/java/JavaAuto/UserService.java
+++ src/main/java/JavaAuto/UserService.java
@@ -1,5 +1,6 @@
 package JavaAuto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -7,6 +8,13 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+//    @Autowired
+//    UserDao userDao;
+//    public String hello() {
+//        return userDao.hello();
+//    }
+
     public List<String> getAllUser() {
         List<String> users = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
